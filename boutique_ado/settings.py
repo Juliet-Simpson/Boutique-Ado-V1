fa416a399885d5ce63a9fe6f863539cc133b8b2e
Juliet-Simpson <simpsonjulietc@gmail.com>
2021-09-20 14:25:30 +0000

    Deployment part 2

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-usvixee!(q9%^nfd0#&hz72=v6kt6f(k9uni$zq4d(b3pfz8gu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['boutique-ad0.herokuapp.com', 'localhost']
 
 
 # Application definition
