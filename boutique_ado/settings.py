01162d4e46b9533669e68e1a5a358262b9004226
Juliet-Simpson <simpsonjulietc@gmail.com>
2021-09-20 14:18:49 +0000

    Deployment part 1 (again)

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -118,20 +118,20 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+Database
+https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
-# DATABASES = {
-#     'default': dj_database_url.parse('postgres://ehvrjrblsdhlnu:86a1035d5c1601f857779b65aaa0515213203e1867dcd4698a2414f45ea3b2cb@ec2-63-32-7-190.eu-west-1.compute.amazonaws.com:5432/dct8n79kqf62eo')
-# }
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
